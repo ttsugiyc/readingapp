c5fee025cce5c7b496ff669aca456aa65293fafd
ttsugiyc <ttsugimoto.yc@kaien-t3.net>
2024-02-01 14:32:16 +0900

    tailwindcss関係ファイルを追加

---
+++ tailwindcss/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./../readingapp/templates/**/*.html"],
+  theme: {
+    body: [
+      'Meiryo',
+    ]
+  },
+  plugins: [],
+}
+
