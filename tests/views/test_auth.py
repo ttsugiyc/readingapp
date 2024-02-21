62783308bede3761c80fd35c3ee661e54f1b58b3
KusaYuno <yzv65cqtnfh9zdso@gmail.com>
2024-02-21 22:40:48 +0900

    [update]本棚画面のテストを追加

--- tests/views/test_auth.py
+++ tests/views/test_auth.py
@@ -32,3 +32,11 @@ def test_register_validate_input(client, username, email, password, message):
         data={'username': username, 'email': email, 'password': password}
     )
     assert message in response.data
+
+
+def test_logout(client, auth):
+    auth.login()
+
+    with client:
+        auth.logout()
+        assert 'user_id' not in session
