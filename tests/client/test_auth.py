7cf70632bb79023fc70ef652fb248c3764e2a0c7
KusaYuno <yzv65cqtnfh9zdso@gmail.com>
2024-02-23 21:53:27 +0900

    [update]最大文字数でのアカウント作成のテストを追加

--- tests/client/test_auth.py
+++ tests/client/test_auth.py
@@ -6,30 +6,38 @@ from flask import testing
 from readingapp.models.database import base
 
 
-def test_register(app: flask.Flask, client: testing.FlaskClient):
+@pytest.mark.parametrize(('username', 'email', 'password'), (
+    ('a', 'a@a', 'a'),
+    ('a', None, 'a'),
+    ('0123456789012345', 'a@a', 'a'),
+    ('a', '01234567890123456789@012345678', 'a'),
+))
+def test_register(app: flask.Flask, client: testing.FlaskClient, username, email, password):
     assert client.get('/auth/register').status_code == 200
     response = client.post(
-        '/auth/register', data={'username': 'a', 'email': 'a@a', 'password': 'a'}
+        '/auth/register',
+        data={'username': username, 'email': email, 'password': password}
     )
     assert response.headers["Location"] == "/auth/login"
 
     with app.app_context():
         assert base.get_database().execute(
-            "SELECT * FROM user WHERE username = 'a'",
+            "SELECT * FROM user WHERE username = ?",
+            (username,)
         ).fetchone() is not None
 
 
 @pytest.mark.parametrize(('username', 'email', 'password', 'message'), (
-    ('', 'b@b', 'b', 'ユーザー名を入力して下さい'.encode()),
-    ('あ', 'b@b', 'b', 'ユーザー名に使用できない文字「あ」が含まれています'.encode()),
-    ('01234567890123456', 'b@b', 'b', 'ユーザー名は16文字以内で入力して下さい'.encode()),
-    ('test', 'b@b', 'b', 'そのユーザー名は既に使用されています'.encode()),
+    ('', 'a@a', 'a', 'ユーザー名を入力して下さい'.encode()),
+    ('あ', 'a@a', 'a', 'ユーザー名に使用できない文字「あ」が含まれています'.encode()),
+    ('01234567890123456', 'a@a', 'a', 'ユーザー名は16文字以内で入力して下さい'.encode()),
+    ('test', 'a@a', 'a', 'そのユーザー名は既に使用されています'.encode()),
 
-    ('b', 'あ@b', 'b', 'メールアドレスに使用できない文字「あ」が含まれています'.encode()),
-    ('b', '01234567890123456789@0123456789', 'b', 'メールアドレスは30文字以内で入力して下さい'.encode()),
-    ('b', 'test@test', 'b', 'そのメールアドレスは既に使用されています'.encode()),
+    ('a', 'あ@a', 'a', 'メールアドレスに使用できない文字「あ」が含まれています'.encode()),
+    ('a', '01234567890123456789@0123456789', 'a', 'メールアドレスは30文字以内で入力して下さい'.encode()),
+    ('a', 'test@test', 'a', 'そのメールアドレスは既に使用されています'.encode()),
 
-    ('b', 'b@b', '', 'パスワードを入力して下さい'.encode()),
+    ('a', 'a@a', '', 'パスワードを入力して下さい'.encode()),
 ))
 def test_register_validate_input(client: flask.Flask, username, email, password, message):
     response = client.post(
