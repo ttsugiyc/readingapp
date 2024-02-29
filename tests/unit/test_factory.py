b5cbd6e009d434bdb17bebb31472b091dd1817c7
KusaYuno <yzv65cqtnfh9zdso@gmail.com>
2024-02-29 20:38:04 +0900

    [clean]微修正

--- tests/unit/test_factory.py
+++ tests/unit/test_factory.py
@@ -3,4 +3,4 @@ import readingapp
 
 def test_config():
     assert not readingapp.create_app().testing
-    assert readingapp.create_app({'None': None}).testing
+    assert readingapp.create_app({}).testing
