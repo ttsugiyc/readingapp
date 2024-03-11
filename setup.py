97664841bfe38dd067bfc04bd0f71b74c48fa8fe
ttsugiyc <ttsugimoto.yc@kaien-t3.net>
2024-03-11 12:57:22 +0900

    [update]バージョンアップ

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='readingapp',
-    version='1.2',
+    version='1.3',
     packages=find_packages(),
     include_package_data=True,
     zip_safe=False,
